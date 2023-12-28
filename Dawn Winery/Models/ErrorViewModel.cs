39b7f581431b9e9a2ccf854a6716724e7b157d84
Fatma <Fatma@DESKTOP-DAHLAUS>
2023-12-28 12:51:09 +0300

    Proje dosyası ekle.

---
+++ Dawn Winery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dawn_Winery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
